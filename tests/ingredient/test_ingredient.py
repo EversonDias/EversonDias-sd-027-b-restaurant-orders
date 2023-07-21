fba36d4f1298686592f62fb4e4d8cf4928399375
everson <eversond14s@gmail.com>
2023-07-21 14:34:24 -0300

    added test for class ingledient

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,16 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import Ingredient, Restriction  # noqa: F401, E261, E501
+
+mock_restrictions = {
+    Restriction.ANIMAL_DERIVED,
+    Restriction.ANIMAL_MEAT
+    }
 
 
 # Req 1
 def test_ingredient():
-    pass
+    recipes = Ingredient("bacon")
+    assert recipes.name == "bacon"
+    assert recipes.__repr__() == "Ingredient('bacon')"
+    assert recipes.restrictions == mock_restrictions
+    assert len(f'{recipes.__hash__()}') == 20
+    assert recipes.__eq__(recipes)
